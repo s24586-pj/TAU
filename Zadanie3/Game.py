6ae98e9899d14d51872449edbaaee3834c6ed4a0
s24586 <s24586@pjwstk.edu.pl>
2024-11-16 19:23:23 +0100

    Dodanie mozliwosci pojedynczego ruchu

--- Zadanie3/Game.py
+++ Zadanie3/Game.py
@@ -3,10 +3,12 @@ import random
 def generuj_plansze(wiersze, kolumny, liczba_przeszkod):
 
     """Tworzenie zarysu mapy"""
+
     plansza = [['#' if i == 0 or i == wiersze - 1 or j == 0 or j == kolumny - 1
                 else ' ' for j in range(kolumny)] for i in range(wiersze)]
 
     """Wylosowanie pozycji startowej jak i mety (A,B),losujemy miejce na pustym polu"""
+
     while True:
         start = (random.randint(1, wiersze - 2), random.randint(1, kolumny - 2))
         if plansza[start[0]][start[1]] == ' ':
@@ -20,6 +22,7 @@ def generuj_plansze(wiersze, kolumny, liczba_przeszkod):
             break
 
     """ Losujemy podobnie jak dla startu i mety (A,B),miejce na przeszkodę (X) """
+
     przeszkody = 0
     while przeszkody < liczba_przeszkod:
         wiersz, kolumna = random.randint(1, wiersze - 2), random.randint(1, kolumny - 2)
@@ -32,12 +35,30 @@ def generuj_plansze(wiersze, kolumny, liczba_przeszkod):
 
 def wypisz_plansze(plansza):
     """Funkcja do wyprintowania planszy"""
+
     print("\n".join(" ".join(wiersz) for wiersz in plansza))
     print("\n")
 
+def wykonaj_ruch(plansza, pozycja, ruch):
+    """Funkcja przyjmuje 3 parametry plansze,pozycje graczaa i ruch w dany kierunek
+        sprawdza czy ruch nie powoduje kolizje ze ścianą # albo z przeszkodą X, jeśli oba warunki przechodzą
+        Zmienia A na nowe pole a starą pozycje zamienia na puste pole
+    """
+    x, y = pozycja
+    dx, dy = ruch
+
+    nowy_x, nowy_y = x + dx, y + dy
+
+    if 0 <= nowy_x < len(plansza) and 0 <= nowy_y < len(plansza[0]):
+        if plansza[nowy_x][nowy_y] not in ('#', 'X'):
+            plansza[x][y] = ' '
+            plansza[nowy_x][nowy_y] = 'A'
+            return nowy_x, nowy_y
+    return pozycja
+
 
 if __name__ == "__main__":
-    """Ustawienia gry,dostosowanie ilości wierszy kolumn i liczby przeszkod"""
+    """Ustawienia gry, dostosowanie ilości wierszy, kolumn i liczby przeszkód"""
     wiersze = 10
     kolumny = 50
     liczba_przeszkod = 70
@@ -45,4 +66,12 @@ if __name__ == "__main__":
     plansza, start, stop = generuj_plansze(wiersze, kolumny, liczba_przeszkod)
 
     wypisz_plansze(plansza)
+
+    pozycja_gracza = start
+
+    pozycja_gracza = wykonaj_ruch(plansza, pozycja_gracza, (0, 1)  )
+
+    wypisz_plansze(plansza)
+    print(f"Nowa pozycja : {pozycja_gracza}")
+
 
