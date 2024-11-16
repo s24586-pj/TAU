aafecd2cb6a35229fb1314f772b1b31c13e5972d
s24586 <s24586@pjwstk.edu.pl>
2024-11-16 19:57:35 +0100

    Petla gry

--- Zadanie3/Game.py
+++ Zadanie3/Game.py
@@ -1,14 +1,12 @@
 import random
+import time
 
 def generuj_plansze(wiersze, kolumny, liczba_przeszkod):
-
     """Tworzenie zarysu mapy"""
-
     plansza = [['#' if i == 0 or i == wiersze - 1 or j == 0 or j == kolumny - 1
                 else ' ' for j in range(kolumny)] for i in range(wiersze)]
 
-    """Wylosowanie pozycji startowej jak i mety (A,B),losujemy miejce na pustym polu"""
-
+    """Wylosowanie pozycji startowej jak i mety (A,B), losujemy miejsce na pustym polu"""
     while True:
         start = (random.randint(1, wiersze - 2), random.randint(1, kolumny - 2))
         if plansza[start[0]][start[1]] == ' ':
@@ -21,8 +19,7 @@ def generuj_plansze(wiersze, kolumny, liczba_przeszkod):
             plansza[stop[0]][stop[1]] = 'B'
             break
 
-    """ Losujemy podobnie jak dla startu i mety (A,B),miejce na przeszkodę (X) """
-
+    """Losujemy podobnie jak dla startu i mety (A,B), miejsce na przeszkodę (X)"""
     przeszkody = 0
     while przeszkody < liczba_przeszkod:
         wiersz, kolumna = random.randint(1, wiersze - 2), random.randint(1, kolumny - 2)
@@ -35,7 +32,6 @@ def generuj_plansze(wiersze, kolumny, liczba_przeszkod):
 
 def wypisz_plansze(plansza):
     """Funkcja do wyprintowania planszy"""
-
     print("\n".join(" ".join(wiersz) for wiersz in plansza))
     print("\n")
 
@@ -54,24 +50,44 @@ def wykonaj_ruch(plansza, pozycja, ruch):
             plansza[x][y] = ' '
             plansza[nowy_x][nowy_y] = 'A'
             return nowy_x, nowy_y
+        else:
+            print('Nie poprawny ruch trafiłeś na przeszkodę lub granice mapys')
     return pozycja
 
 
 if __name__ == "__main__":
-    """Ustawienia gry, dostosowanie ilości wierszy, kolumn i liczby przeszkód"""
+    """Ustawienia gry, dostosowanie ilości wierszy, kolumn i liczby przeszkód
+    oraz pętla gry,opiera się na sprawdzeniu czy gracz dotarł do mety jesli nie 
+     przyjmuje kierunek gracza a nastepnie odpala funkcje wykonaj_ruch
+     na sam koniec liczy czas"""
     wiersze = 10
-    kolumny = 50
-    liczba_przeszkod = 70
-
+    kolumny = 10
+    liczba_przeszkod = 3
+    start_gry = time.time()
     plansza, start, stop = generuj_plansze(wiersze, kolumny, liczba_przeszkod)
 
-    wypisz_plansze(plansza)
-
     pozycja_gracza = start
-
-    pozycja_gracza = wykonaj_ruch(plansza, pozycja_gracza, (0, 1)  )
-
     wypisz_plansze(plansza)
-    print(f"Nowa pozycja : {pozycja_gracza}")
 
+    while pozycja_gracza != stop:
+        ruch = input("Wybierz ruch (w, a, s, d): ").strip().lower()
 
+        if ruch == 'w':
+            pozycja_gracza = wykonaj_ruch(plansza, pozycja_gracza, (-1, 0))
+        elif ruch == 's':
+            pozycja_gracza = wykonaj_ruch(plansza, pozycja_gracza, (1, 0))
+        elif ruch == 'a':
+            pozycja_gracza = wykonaj_ruch(plansza, pozycja_gracza, (0, -1))
+        elif ruch == 'd':
+            pozycja_gracza = wykonaj_ruch(plansza, pozycja_gracza, (0, 1))
+        else:
+            print("Niepoprawny ruch. Spróbuj ponownie.")
+            continue
+
+        wypisz_plansze(plansza)
+        koniec_gry = time.time()
+        czas_trwania = koniec_gry - start_gry
+        print(f"Pozycja gracza: {pozycja_gracza}")
+
+    print("===========KONIEC GRY==============")
+    print(f"Gra trwała: {czas_trwania:.2f} sekund.")
